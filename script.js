711354ba1cca5f0a960d4f1f18b575e85209f949
LalatenduBhatta <lalatendubhatta@gmail.com>
2023-11-07 19:46:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// ============================Typing Animation===============
+var typed= new Typed('.typing',{
+    strings:['','Web Designer','Frontend Web Developer','Full Stack Web Developer'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
